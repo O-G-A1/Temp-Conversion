6f41cd19c88539b07cef4f4c417734a91ea1d124
Gbenga Ogunsola <gbengaadeboye99@gmail.com>
2024-08-28 17:41:26 +0100

    temp-conversion

---
+++ index.js
@@ -0,0 +1,25 @@
+let checkCelsius=document.getElementById("toCelsius");
+let checkFahrenheit=document.getElementById("toFahrenheit");
+const result=document.querySelector(".result");
+
+
+
+function convert(){    
+    if(checkCelsius.checked){
+            const valueToConvert=document.getElementById("toconvert").value;
+            const toCelsius=((valueToConvert-32)*5)/9;
+            const outcome=`The conversion of ${valueToConvert}°F is ${toCelsius}°C`;
+            result.textContent=outcome;
+            
+        }
+        else if(checkFahrenheit.checked){
+            const valueToConvert=document.getElementById("toconvert").value;
+            const toFahrenheit=((valueToConvert*9)/5)+32;
+            const outcome=`The conversion of ${valueToConvert}°C is ${toFahrenheit}°F`;
+            result.textContent=outcome;
+
+            
+
+    }  
+
+}
